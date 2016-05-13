39df8f6f9edcb17d70b54b0741d9a166c939b311
manelizzard <manelizzard@gmail.com>
2016-05-13 10:46:45 +0200

    * ADD back button and scenes into build settings

--- Guess The Number - UI/Assets/Scripts/LevelManager.cs
+++ Guess The Number - UI/Assets/Scripts/LevelManager.cs
@@ -5,9 +5,11 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log ("Load level requested for: " + name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest() {
 		Debug.Log ("Quit request");
+		Application.Quit ();
 	}
 }
